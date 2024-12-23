9884f29e751155ce9db4471d2f04dc29f0f31e38
ali <fiharali0@gmail.com>
2024-12-23 14:38:00 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TodoComponent } from "./todo/todo.component";
-import { NavbarComponent } from "./navbar/navbar.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TodoComponent, NavbarComponent],
+  imports: [RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
