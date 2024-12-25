14201f76aaca25b4e60f5e8cfabca030faeed926
ali <fiharali0@gmail.com>
2024-12-25 11:59:31 +0100

    add structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { AppComponent } from './app.component';
-import { authGuard } from '../guard/auth.guard';
-import { redirectIfAuthenticatedGuard } from '../guard/redirectIfAuthenticatedGuard';
+import { authGuard } from './guard/auth.guard';
+import { redirectIfAuthenticatedGuard } from './guard/redirectIfAuthenticatedGuard';
 import { StatistiquesComponent } from './components/statistiques/statistiques.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 
