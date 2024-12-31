13d3617a3f8ce026cd7a790d89fe065d11d2786a
ali <fiharali0@gmail.com>
2024-12-31 12:12:55 +0100

    add species crud

--- src/app/components/species/create/speciesCreate.component.ts
+++ src/app/components/species/create/speciesCreate.component.ts
@@ -34,25 +34,20 @@ export class SpeciesCreateComponent {
   constructor( private fb: FormBuilder, private api: ApiService ,  private router: Router) {
 
     this.speciesForm = this.fb.group({
-      speciesType: ['',[ Validators.required ]],
-      date: ['', [Validators.required ]],
-      code: ['', [Validators.required , Validators.minLength(3)]],
-      openRegistration: ['', [Validators.required]],
-      location: ['',[ Validators.required , Validators.minLength(5)]],
-      minParticipants: ['', [Validators.required , Validators.min(1)]],
-      maxParticipants: ['', [Validators.required]],
-
+      name: ['',[ Validators.required ]],
+      category: ['', [Validators.required ]],
+      minimumWeight: ['', [Validators.required  , Validators.min(0)]],
+      difficulty: ['', [Validators.required ]],
+      points: ['', [Validators.required , Validators.min(0)]],
     });
 
   }
 
-  get speciesType() { return this.speciesForm.get('speciesType'); }
-  get date() { return this.speciesForm.get('date'); }
-  get code() { return this.speciesForm.get('code'); }
-  get openRegistration() { return this.speciesForm.get('openRegistration'); }
-  get location() { return this.speciesForm.get('location'); }
-  get minParticipants() { return this.speciesForm.get('minParticipants'); }
-  get maxParticipants() { return this.speciesForm.get('maxParticipants'); }
+  get name() { return this.speciesForm.get('name'); }
+  get category() { return this.speciesForm.get('category'); }
+  get minimumWeight() { return this.speciesForm.get('minimumWeight'); }
+  get difficulty() { return this.speciesForm.get('difficulty'); }
+  get points() { return this.speciesForm.get('points'); }
 
 
 
@@ -60,7 +55,7 @@ export class SpeciesCreateComponent {
   onSubmit() {
     if (this.speciesForm.valid) {
       const speciesData = this.speciesForm.value;
-      this.api.post<Species>('/speciess', speciesData).subscribe({
+      this.api.post<Species>('/species', speciesData).subscribe({
         next: (newCompetition) => {
           console.log('species created:', newCompetition);
           this.species.push(newCompetition);
@@ -72,13 +67,7 @@ export class SpeciesCreateComponent {
           });
         },
         error: (err) => {
-
-          if (err?.error?.maxGreaterThanMin) {
-            this.speciesForm.get('maxParticipants')?.setErrors({
-              serverError: err.error.maxGreaterThanMin
-            });
-          }
-          console.error('Error creating species:', err.error.maxGreaterThanMin);
+          console.error('Error creating species:', err);
         }
       });
     } else {
